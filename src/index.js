66fadf425d82acd8077aaa47660725db59cebcec
Varad Gupta <varadgupta21@gmail.com>
2023-11-12 16:42:38 +0530

    Added Auth Validation Middleware

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser')
 
 const apiRoutes  = require('./routes/index')
 
-const UserService = require('./services/user-service')
 
 const app = express()
 
@@ -15,12 +14,6 @@ const prepareAndStartServer  = ()=>{
 
   app.listen(Port,()=>{
     console.log(`Server Started on port ${Port}`)
-    const userService = new UserService()
-    // const token = userService.createToken({email :"varad@gmail", id: 1})
-    // console.log(token)
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZhcmFkQGdtYWlsIiwiaWQiOjEsImlhdCI6MTY5OTc4NDcwMywiZXhwIjoxNjk5Nzg4MzAzfQ.mCyG3yf0q8dYlgmVCpKT8mmczWM6I7txBamfOqzIt-g"
-    const user = userService.verifyToken(token)
-    console.log(user)
   })
 }
 
